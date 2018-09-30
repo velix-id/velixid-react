8c36119b96b9ab515077cb0da3666578765771c0
Balwant Singh <bsingh@Balwants-COG-MacBook-Pro.local>
2018-09-30 14:56:16 +1000

    Used VelixID js in the back Event System working

--- src/lib/LoginButton.js
+++ src/lib/LoginButton.js
@@ -1,56 +1,44 @@
 import React from 'react';
 import './LoginButton.css';
 
+import VelixID from "velixid.js";
+
 let _onStateChange = function () { };
 let _velixIDPartnerToken = null;
 
-function openPopupCenter(url, title, w, h) {
-  // Fixes dual-screen position
-  // Most browsers use window.screenLeft
-  // Firefox uses screen.left
-  var dualScreenLeft = getFirstNumber(window.screenLeft, screen.left),
-    dualScreenTop = getFirstNumber(window.screenTop, screen.top),
-    width = getFirstNumber(window.innerWidth, document.documentElement.clientWidth, screen.width),
-    height = getFirstNumber(window.innerHeight, document.documentElement.clientHeight, screen.height),
-    left = ((width / 2) - (w / 2)) + dualScreenLeft,
-    top = ((height / 2) - (h / 2)) + dualScreenTop,
-    newWindow = window.open(url, title, getSpecs());
-
-  // Puts focus on the newWindow
-  if (window.focus) {
-    newWindow.focus();
-  }
-
-  return newWindow;
-
-  function getSpecs() {
-    return 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left;
-  }
-
-  function getFirstNumber() {
-    for (var i = 0, len = arguments.length; i < len; i++) {
-      var value = arguments[i];
-
-      if (typeof value === 'number') {
-        return value;
-      }
-    }
-  }
-}
-
-let _openLoginPopup = (token) => {
-  this._popup = openPopupCenter('https://api.velix.id/popup/login/' + token, 'Login with Velix.ID', 500, 500);
-}
+VelixID.on(VelixID.EVENTS.LOGIN, (data) => {
+  _onStateChange({
+    state: VelixID.EVENTS.LOGIN,
+    event: data
+  })
+});
+VelixID.on(VelixID.EVENTS.ERROR, (data) => {
+  _onStateChange({
+    state: VelixID.EVENTS.ERROR,
+    event: data
+  })
+});
+VelixID.on(VelixID.EVENTS.REJECT, (data) => {
+  _onStateChange({
+    state: VelixID.EVENTS.REJECT,
+    event: data
+  })
+});
+VelixID.on(VelixID.EVENTS.CANCEL, (data) => {
+  _onStateChange({
+    state: VelixID.EVENTS.CANCEL,
+    event: data
+  })
+});
 
 function onClick(event) {
-
   _onStateChange({
     state: 'login-init',
     event: event
   });
 
   if (_velixIDPartnerToken) {
-    _openLoginPopup(_velixIDPartnerToken)
+    VelixID._openLoginPopup(_velixIDPartnerToken)
   } else {
     _onStateChange({
       state: 'login-failed',
@@ -59,9 +47,12 @@ function onClick(event) {
   }
 }
 
-
 const LoginButton = ({ label = 'Login with Velix.ID', token, onStateChange = () => { } }) => {
   _velixIDPartnerToken = token;
+  VelixID.init({
+    token: token,
+    disableButtonSetup: true
+  })
   _onStateChange = onStateChange;
   return <button data-button-type="velixid-login" className="velixid-button-primary" onClick={onClick}>{label}</button>
 }
